7225d39c41f6422635c152baf9467ca739d79be1
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-06 15:44:57 +0530

    pingpong

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get("/ping",(req,res)=>{
     res.send("pong");
 })
 
+app.get("/get-info-query/:id/:password",(req,res)=>{
+    try {
+        console.log(req.params);
+        res.send("successfully")
+    } catch (error) {
+        res.status(500).send({message:"some thing went wrong"})
+    }
+})
 app.listen(89,()=>{
     console.log("Server connected successfully")
 })
